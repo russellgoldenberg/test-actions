5b0706799d1d2662fb727031ffb419234720d61a
russellgoldenberg <russellgoldenberg@gmail.com>
2020-12-16 10:12:01 -0500

    real change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 
 const random = Math.random();
-fs.writeFileSync("./index.html", `<html><head></head><body><h1>Test</h1><p>${random}</p></body></html>`);
+fs.writeFileSync("./index.html", `<html><head></head><body><h1>A real change</h1><p>${random}</p></body></html>`);
